12a4d667b56a720a86d6ac15c47aa9015b39d5ea
naisargmodi <31739279+naisargmodi@users.noreply.github.com>
2017-10-16 17:38:33 -0400

    Add files via upload

---
+++ 01_01_Hello World.py
@@ -0,0 +1,8 @@
+print("Hello Friends")
+input()
+
+Friend1 = input("How you doing?")
+
+Friend2 = input("I'm doing wonderful! What brings you to python class?")
+
+Friend1 = input("That's awesome! Why are you interested in learning python?")
